811a3761b3b83d2341cfcd11221657c4a3e6002e
lushangyan <765277920@qq.com>
2022-01-16 21:45:34 +0800

    第二周http请求作业提交

---
+++ 02nio/homework02/src/main/java/work/AllGet.java
@@ -0,0 +1,18 @@
+package work;
+
+import utils.HttpClientWork;
+import utils.OkHttpWork;
+
+import java.io.IOException;
+
+public class AllGet {
+    public static void main(String[] args) throws IOException {
+        String url = "http://localhost:8801";
+        String httpClientResponse = new HttpClientWork().get(url);
+        System.out.println("httpClientResponse" + httpClientResponse);
+
+        String okHttpResponse = new OkHttpWork().get(url);
+        System.out.println("okHttpResponse:" + okHttpResponse);
+    }
+
+}
